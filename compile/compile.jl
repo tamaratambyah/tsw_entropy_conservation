b67e783a2690e9f9397f1b6ceaf2133580b119b2
Tamara Tambyah <tamara.tambyah@monash.edu>
2024-07-29 10:48:20 +1000

    removing compile

--- compile/compile.jl
+++
@@ -1,5 +0,0 @@
-using PackageCompiler
-
-create_sysimage([:PoissonIntegrator],
-  sysimage_path=joinpath(@__DIR__,"..","PoissonIntegrator.so"),
-  precompile_execution_file=joinpath(@__DIR__,"warmup.jl"))
